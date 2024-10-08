2558ff35a58f8ffc35538b64dbe224155d76f981
pavanik26 <pavani.kashana26@gmail.com>
2024-10-08 14:35:20 +0530

    Initial commit

---
+++ src/main/java/jatTask18/jatTask18/Program1.java
@@ -0,0 +1,103 @@
+package jatTask18.jatTask18;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+	       
+	       // Initialize FirefoxDriver with options
+	       WebDriver driver = new ChromeDriver();
+	       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+           driver.get("https://www.facebook.com");
+           
+           // Maximize the window
+           driver.manage().window().maximize();
+           
+           waitForElement(4000);
+           
+           if(driver.findElements(By.xpath("//img[@alt='Facebook']")).size()>0)
+           {
+           System.out.println("landed on correct page");
+           }
+           else
+           {
+         	  System.out.println("landed on incorrect page");
+           }  
+           
+           
+           //click on Create new account
+           WebElement createNewAccount = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Create new account')]")));                   
+           createNewAccount.click();
+           
+           WebElement firstName = driver.findElement(By.xpath("//input[@name='firstname']"));
+           firstName.sendKeys("testfname");
+           
+           WebElement lastName = driver.findElement(By.xpath("//input[@name='lastname']"));
+           lastName.sendKeys("testlname");
+           
+           WebElement day = driver.findElement(By.xpath("//select[@id='day']"));
+           day.sendKeys("10");
+           
+           WebElement month = driver.findElement(By.xpath("//select[@id='month']"));
+           month.sendKeys("October");
+           
+           WebElement year = driver.findElement(By.xpath("//select[@id='year']"));
+           year.sendKeys("1990");
+           
+           WebElement gender = driver.findElement(By.xpath("(//input[@id='sex'])[1]"));
+           gender.click();
+           
+           WebElement email = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+           email.sendKeys("testfname710@gmail.com");
+           
+           WebElement pwd = driver.findElement(By.xpath("//input[@id='password_step_input']"));
+           pwd.sendKeys("India@12345");
+           
+           WebElement btnSignUp = driver.findElement(By.xpath("//button[contains(text(),'Sign Up')]"));
+           btnSignUp.click(); 
+           
+          wait.until(ExpectedConditions.urlContains("facebook.com/?ref=home"));
+           
+           waitForElement(4000);
+           
+           // Check the current URL to confirm redirection
+           String currentUrl = driver.getCurrentUrl();
+           if (currentUrl.equals("https://www.facebook.com/")) {
+               System.out.println("User successfully registered and redirected to the homepage.");
+           } else {
+               System.out.println("User registration failed or not redirected correctly.");
+           }
+           
+           waitForElement(4000);
+           driver.quit();
+           
+	} 
+           
+       public static void waitForElement(int milliseconds)
+       	{
+       		try {
+       			Thread.sleep(milliseconds);
+       		} catch (InterruptedException e) {
+       			// TODO Auto-generated catch block
+       			e.printStackTrace();
+       		}
+       	} 
+
+
+	}
+
+
---
+++ src/main/java/jatTask18/jatTask18/Program2.java
@@ -0,0 +1,86 @@
+package jatTask18.jatTask18;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Program2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+	       
+	       // Initialize FirefoxDriver with options
+	       WebDriver driver = new ChromeDriver();
+	      
+	       try
+	       {
+           driver.get("https://jqueryui.com/droppable/");
+           
+           // Maximize the window
+           driver.manage().window().maximize();
+           
+           waitForElement(4000);
+           
+          WebElement elm = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
+           driver.switchTo().frame(elm);
+          Actions action = new Actions(driver);
+          WebElement source = driver.findElement(By.xpath("//div[@id='draggable']"));
+          WebElement target = driver.findElement(By.xpath("//div[@id='droppable']"));
+          
+          action.dragAndDrop(source, target).build().perform();    
+          waitForElement(4000);          
+          
+
+          String expectedColor = "rgba(255, 250, 144, 1)";
+
+          String rgbaActualColor = driver.findElement(By.xpath("//div[@id='droppable']"))
+                                         .getCssValue("background-color");
+          
+                  
+          if (rgbaActualColor.contains(expectedColor) && (driver.findElements(By.xpath("//p[contains(text(),'Dropped!')]")).size()>0))
+          {
+        	  System.out.println("background color of target element changed and text of target element changed to Dropped!");
+          }
+          
+          else
+          {
+        	  System.out.println("background color of target element not changed and text of target element not changed");
+          }
+
+          driver.switchTo().defaultContent();
+           
+       } 
+	       
+	   catch(Exception e)
+	       {
+		// TODO Auto-generated catch block
+					e.printStackTrace();
+	       }
+			   
+		   finally {
+			   }
+           // Close the browser
+           driver.quit();
+       }
+
+	
+	   public static void waitForElement(int milliseconds)
+   	   {
+   		  try {
+   			Thread.sleep(milliseconds);
+   		} catch (InterruptedException e) {
+   			// TODO Auto-generated catch block
+   			e.printStackTrace();
+   		}
+   	} 
+
+}
